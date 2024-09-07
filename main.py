441bcf24f872670c98e29a1ef9238a8fed89f6eb
simon <sim386007@gmail.com>
2024-09-07 21:48:17 +0300

    optimization

--- main.py
+++ main.py
@@ -4,11 +4,6 @@ from pygame.locals import *
 pygame.init()
 
 myfont = pygame.font.SysFont("monospace", 36)
-formiga = []
-inte = 0
-formiga.append(45)
-formiga.append(45)
-formiga.append(1)
 
 black = (0, 0, 0)
 white = (255, 255, 255)
@@ -25,8 +20,7 @@ screen = pygame.display.set_mode((width, height))
 COLS, ROWS = width // cell_size, height // cell_size
 grid = [[0 for col in range(COLS)]for row in range(ROWS)]
 
-x_pos = 1
-y_pos = 1
+rot = [(width//10)//2, (height//10)//2, 1]
 
 while True:
 
@@ -43,36 +37,36 @@ while True:
             else:
                 pygame.draw.rect(screen, (255, 255, 255), (x_pos*10, y_pos*10, cell_size, cell_size))     
 
-    if(grid[formiga[1]][formiga[0]] == 0):
-        grid[formiga[1]][formiga[0]] = 1
-        formiga[2] = (formiga[2] + 1) % 4
-        if formiga[2] == 0:
-            formiga[0] -= 1
-        if formiga[2] == 1:
-            formiga[1] += 1
-        if formiga[2] == 2:
-            formiga[0] += 1
-        if formiga[2] == 3:
-            formiga[1] -= 1
+    if(grid[rot[1]][rot[0]] == 0):
+        grid[rot[1]][rot[0]] = 1
+        rot[2] = (rot[2] + 1) % 4
+        if rot[2] == 0:
+            rot[0] -= 1
+        if rot[2] == 1:
+            rot[1] += 1
+        if rot[2] == 2:
+            rot[0] += 1
+        if rot[2] == 3:
+            rot[1] -= 1
         steps += 1
 
-    if(grid[formiga[1]][formiga[0]] == 1): 
-        grid[formiga[1]][formiga[0]] = 0
-        formiga[2] = (formiga[2] - 1) % 4
-        if formiga[2] == 0:
-            formiga[0] -= 1
-        if formiga[2] == 1:
-            formiga[1] += 1
-        if formiga[2] == 2:
-            formiga[0] += 1
-        if formiga[2] == 3:
-            formiga[1] -= 1 
+    if(grid[rot[1]][rot[0]] == 1): 
+        grid[rot[1]][rot[0]] = 0
+        rot[2] = (rot[2] - 1) % 4
+        if rot[2] == 0:
+            rot[0] -= 1
+        if rot[2] == 1:
+            rot[1] += 1
+        if rot[2] == 2:
+            rot[0] += 1
+        if rot[2] == 3:
+            rot[1] -= 1 
         steps += 1
 
     strings = str(steps)
-    label = myfont.render(strings, 1, (255,255,255))
+    steps_bar = myfont.render(strings, 1, (255,255,255))
   
     screen.unlock()
-    screen.blit(label, (0,0))
+    screen.blit(steps_bar, (0,0))
     pygame.display.update()
     clock.tick(120)
